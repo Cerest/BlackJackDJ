c5943f2f02b83464fc2794ee1460caf67f8d294d
Daniel Nabours <danielnabours@gmail.com>
2020-07-28 16:45:37 -0500

    Created Deck, Hand, and Card objects
    included methods for interacting with them. to access the objects, use Model.Deck, Model.Card, or Model.Hand

--- Model.cs
+++ Model.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ namespace ACNS_Blackjack
 {
     class Model
     {
-        class Card
+        public class Card
         {
             #region ctor
             public Card()
@@ -21,30 +22,7 @@ namespace ACNS_Blackjack
             }
             #endregion
             #region properties
-            public enum CardValue
-            {
-                Ace = 1,
-                Two = 2,
-                Three = 3,
-                Four = 4,
-                Five = 5,
-                Six = 6,
-                Seven = 7,
-                Eight = 8,
-                Nine = 9,
-                Ten = 10,
-                Jack = 11,
-                Queen = 12,
-                King = 13
-            }
-
-            public enum CardSuit
-            {
-                Hearts = 1,
-                Spades = 2,
-                Clubs = 3,
-                Diamonds = 4
-            }
+            
 
             Image image;
             CardValue cardValue;
@@ -123,5 +101,133 @@ namespace ACNS_Blackjack
             }
             #endregion
         }
+        public class Hand
+        {
+            private List<Card> cards;
+
+            public List<Card> Cards
+            {
+                get { return cards; }
+            }
+
+            public Hand(int startingHand, Deck deck)
+            {
+                if (deck == null) throw new DeckException("No decks available to draw from!");
+                else if (deck.Cards.Count == 0) throw new DeckException("No more cards to draw!");
+                else
+                {
+                    cards = new List<Card>();
+                    for (int i = 0; i < startingHand; i++)
+                    {
+                        deck.DrawCard(this);
+                    }
+                }
+            }
+
+            public void AddValue(Card drawn, ref int currentSum)
+            {
+                if (drawn.Value == CardValue.Ace)
+                {
+                    if (currentSum <= 10)
+                    {
+                        currentSum += 11;
+                    }
+                    else
+                    {
+                        currentSum += 1;
+                    }
+                }
+                else if (drawn.Value == CardValue.Jack || drawn.Value == CardValue.Queen || drawn.Value == CardValue.King)
+                {
+                    currentSum += 10;
+                }
+                else
+                {
+                    currentSum += (int)drawn.Value;
+                }
+            }
+        }
+        public class Deck
+        {
+            private List<Card> cards;
+
+            public List<Card> Cards
+            {
+                get { return this.cards; }
+                set { this.cards = value; }
+            }
+            public Deck()
+            {
+                Cards = new List<Card>();
+                ShuffleNewDeck();
+            }
+
+            public void ShuffleNewDeck()
+            {
+                cards.Clear();
+                for (int i = 1; i < 5; i++)//CardSuits
+                {
+                    for (int j = 1; j < 14; j++)//CardValues: 2...10,J,Q,K,A = 13 different values 
+                    { Card card = new Card(); 
+                        card.Value = (CardValue)j; 
+                        card.Suit = (CardSuit)i; 
+                        cards.Add(card); 
+                    } 
+                } 
+                Random r = new Random(); 
+                cards = cards.OrderBy(x => r.Next()).ToList();
+            }
+            public Card DrawCard(Hand hand)
+            {
+                Card drawn = cards[cards.Count - 1];
+                cards.Remove(drawn);
+                hand.Cards.Add(drawn);
+                return drawn;
+            }
+        }
+        public enum CardValue
+            {
+                Ace = 1,
+                Two = 2,
+                Three = 3,
+                Four = 4,
+                Five = 5,
+                Six = 6,
+                Seven = 7,
+                Eight = 8,
+                Nine = 9,
+                Ten = 10,
+                Jack = 11,
+                Queen = 12,
+                King = 13
+            }
+        public enum CardSuit
+            {
+                Hearts = 1,
+                Spades = 2,
+                Clubs = 3,
+                Diamonds = 4
+            }
+        #region custom exceptions
+        [Serializable]
+        internal class DeckException : Exception
+        {
+            public DeckException()
+            {
+            }
+
+            public DeckException(string message) : base(message)
+            {
+            }
+
+            public DeckException(string message, Exception innerException) : base(message, innerException)
+            {
+            }
+
+            protected DeckException(SerializationInfo info, StreamingContext context) : base(info, context)
+            {
+            }
+        }
+        #endregion
     }
 }
