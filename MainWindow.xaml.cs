c4153fe584e8fa53e9b5ec594a3104ed1feab2de
Daniel Nabours <danielnabours@gmail.com>
2020-07-30 15:26:48 -0500

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,23 @@ namespace ACNS_Blackjack
     /// </summary>
     public partial class MainWindow : Window
     {
+        Controller controller;
         public MainWindow()
         {
             InitializeComponent();
+            controller = new Controller();
+        }
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            Storyboard sb = new Storyboard();
+            DoubleAnimation ani = new DoubleAnimation();
+            ani.From = 1;
+            ani.To = 0;
+            ani.Duration = new Duration(new TimeSpan(0,0,0,2));
+            Storyboard.SetTarget(ani, TitleScreen);
+            Storyboard.SetTargetProperty(ani, new PropertyPath(Grid.OpacityProperty));
+            sb.Children.Add(ani);
+            sb.Begin();
         }
     }
 }
